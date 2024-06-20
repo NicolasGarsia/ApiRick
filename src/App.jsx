1a59caed9c58652a0048ef681c31751d722e329c
NICOLAS SCHNEIDER LAPOLLI GARCIA <nicolas_s_garcia@estudante.sesisenai.org.br>
2024-06-20 11:50:05 -0300

    Rick1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import RickAndMorty from './components/PerRick'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+    <RickAndMorty />
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/PerRick.jsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react";
+
+export default function RickAndMorty() {
+    const [name, setName] = useState("");
+    const [char, setChar] = useState(null);
+    const [error, setError] = useState(null);
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch(`https://rickandmortyapi.com/api/character/?name=${name}`);
+            const data = await response.json();
+            if (data.results && data.results.length > 0) {
+                setChar(data.results[0]);
+                setError(null); // Limpa o erro se a busca tiver sucesso
+            } else {
+                setChar(null);
+                setError("Personagem não encontrado");
+            }
+        } catch (error) {
+            console.log("Erro", error);
+            setError("Erro ao buscar personagem");
+        }
+    };
+
+    const handleSearch = () => {
+        if (name !== "") {
+            fetchData();
+        } else {
+            setError("Digite o nome do personagem para buscar.");
+        }
+    };
+
+    const handleInputChange = (event) => {
+        setName(event.target.value);
+    };
+
+    return (
+        <div id="box">
+            <nav id="banner"><h1> Characters Of RickAndMorty </h1> </nav>
+        <div>
+            <div id="busca">
+            <input  type="text" value={name} name="name" onChange={handleInputChange} />
+            <button onClick={handleSearch}>Buscar</button>
+            </div>
+            {char && (
+                <div className="Rick">
+                    <p id="name">{char.name}</p>
+                    <img src={char.image} alt={char.name} />
+                </div>
+            )}
+        </div>
+        </div>
+    );
+}
